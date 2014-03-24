3896e8346435e9da38819088983b2c9609591fae
goodgoblin <alex@schoolchapters.com>
2014-03-24 13:50:57 -0400

    Fail conversion gracefully and track failure in model state for eventual retry

--- lib/carrierwave-unoconv/unoconv.rb
+++ lib/carrierwave-unoconv/unoconv.rb
@@ -12,12 +12,19 @@ module CarrierWave
       tmpfile   = File.join( directory, "tmpfile" )
   
       File.rename( current_path, tmpfile )
-        
-      raise CarrierWave::ProcessingError, "UNOconv failed." unless system "unoconv -f #{format} '#{tmpfile}'"
-      
-      File.rename( File.join(directory, "tmpfile.#{format}"), current_path )
-  
-      File.delete( tmpfile )
+      begin
+        system "unoconv -f #{format} '#{tmpfile}'"
+        File.rename( File.join(directory, "tmpfile.#{format}"), current_path )
+        File.delete( tmpfile )
+        if model.respond_to?('pdf_encoding_state')
+          model.pdf_encoding_state = 1
+        end
+      rescue Exception=>e
+        if model.respond_to?('pdf_encoding_state')
+          model.pdf_encoding_state = 2
+        end
+        Rails.logger.error("Error converting file #{current_path} #{e}")
+      end  
     end
   end
 end
