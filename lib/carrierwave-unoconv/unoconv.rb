150dc05a6dcbbd9213635d73f7ad05898e193800
mtmorra <mtmorra@gmail.com>
2014-05-08 10:14:45 -0400

    Removed extra set of quotes around tmpfile

--- lib/carrierwave-unoconv/unoconv.rb
+++ lib/carrierwave-unoconv/unoconv.rb
@@ -15,7 +15,7 @@ module CarrierWave
       begin
         Rails.logger.debug("BEGIN CONVERT!!!")
         Timeout.timeout(0.0001) do
-          pid = %x[unoconv -f "#{format}" "'#{tmpfile}'" & echo $!]
+          pid = %x[unoconv -f "#{format}" "#{tmpfile}" & echo $!]
           Rails.logger.debug("PID IS #{pid}")
           #system "unoconv -f #{format} '#{tmpfile}'"
           File.rename( File.join(directory, "tmpfile.#{format}"), current_path )
